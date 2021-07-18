1a6218af7385b57acda73dafb710fae643c17d4f
Hillary Omondi <79520103+Hilix@users.noreply.github.com>
2021-07-19 01:13:12 +0300

    Update Asignment1.py

--- Asignment1.py
+++ Asignment1.py
@@ -1,2 +1,43 @@
-'hello world'
+## 1. Compose a program that writes the Hello, World message 10 times. 	
+
+for i in range (10):
+print("hello world")
+
+	 
+
+## 2. Describe what happens if you omit the following in helloworld.py: 
+•	the first '  
+Program returns a syntax error; invalid syntax
+			  
+
+•	the second '  
+Program returns syntax error; EOL while scanning string literal
+			  
+
+•	the print() statement  
+The program does not print helloworld
+			   
+
+## 3. Which one of the following is the correct way to execute useargument.py using the terminal: •
+
+python useargument.py python
+
+python3 useargument.py
+
+python useargument.py
+
+useargument.py python
+
+python useargument	
+
+
+
+python3 useargument.py
+python useargument.py
+	   
+
+
+
+## 4. Modify helloworld.py to compose a program that takes three names and writes a proper sentence with the names in the reverse of the order they are given, so that, for example, python helloworld.py Alice Bob Carol writes the string 'Hi Carol, Bob, and Alice'
+
 
